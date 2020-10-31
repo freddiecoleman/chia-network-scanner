5a99f0bf93d96d0087bfbe0ee7685561f088d646
Freddie Coleman <f.coleman@hotmail.co.uk>
2020-10-31 12:54:36 +0000

    network scanner config options parser

--- options.ts
+++ options.ts
@@ -1,3 +1,5 @@
+import * as z from 'zod';
+
 type NetworkId = 'testnet' | 'mainnet'; // There will eventually be 3 networks
 type ProtocolVersion = '0.0.18'; // For now we will only support one protocol at a time and try to keep up to date
 
@@ -15,15 +17,15 @@ interface NodeOptions {
 interface NetworkOptions {
     networkId: NetworkId;
     protocolVersion: ProtocolVersion;
-    connectionTimeout: number; // Time within which a peer much respond to peer protocol handshake before bailing
+    // Time within which a peer much respond to peer protocol handshake before bailing in ms.
+    connectionTimeout: number;
 }
 
 /**
  * Details for this peer as it connects to the Chia network.
  */
 interface PeerOptions {
-    // 32 bytes
-    nodeId: Buffer;
+    nodeId: string;
     nodeType: number;
 }
 
@@ -36,9 +38,40 @@ interface NetworkScannerOptions {
     peer: PeerOptions;
 }
 
-// Todo: use zod to parse options and ensure valid values
-const parseOptions = (options: NetworkScannerOptions) => {
+const nodeOptionsSchema = z.object({
+    hostname: z.string(),
+    port: z
+        .number()
+        .min(0)
+        .max(65535)
+});
 
-};
+const networkOptionsSchema = z.object({
+    networkId: z.union([
+        z.literal('testnet'),
+        z.literal('mainnet')
+    ]),
+    protocolVersion: z.literal('0.0.18'),
+    connectionTimeout: z
+        .number()
+        .min(250)
+        .max(30000)
+});
+
+const peerOptionsSchema = z.object({
+    nodeId: z
+        .string()
+        .min(32)
+        .max(32),
+    nodeType: z.number() // Todo: improve validation of this
+});
+
+const networkScannerOptionsSchema = z.object({
+    node: nodeOptionsSchema,
+    network: networkOptionsSchema,
+    peer: peerOptionsSchema
+});
+
+const parseOptions = (options: NetworkScannerOptions): NetworkScannerOptions => networkScannerOptionsSchema.parse(options);
 
 export { NetworkScannerOptions };
