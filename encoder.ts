fb3af8ee0fa30472d5f31f80d8063a5ba1660386
Freddie Coleman <f.coleman@hotmail.co.uk>
2021-01-14 20:23:34 +0000

    remove length from start of bytes sent over the wire

--- encoder.ts
+++ encoder.ts
@@ -4,10 +4,9 @@ import { log } from './log';
 import { Message } from './MessageChannel';
 
 const decodePeer = (peer: Buffer): Peer => {
-    const length = peer.readUIntBE(0, 4);
-    const hostname = peer.slice(4, length + 4).toString();
-    const port = peer.readUIntBE(length + 4, 2);
-    const timestamp = parseInt(peer.readBigUInt64BE(length + 6).toString(), 10);
+    const hostname = peer.slice(0, 4).toString();
+    const port = peer.readUIntBE(4, 2);
+    const timestamp = parseInt(peer.readBigUInt64BE(6).toString(), 10);
 
     return new Peer({
         hostname,
@@ -19,31 +18,20 @@ const decodePeer = (peer: Buffer): Peer => {
 const encodeMessage = (func: string, data: any): Buffer => {
     const message = cbor.encode({
         f: func,
-        d: data
+        d: data,
+        i: Buffer.from('test') // Todo: temporarily doing this to see if it fixes the current error
     }) as Buffer;
 
-    const length = Buffer
-        .alloc(4)
-        .fill(0);
-
-    length.writeUIntBE(message.byteLength, 0, 4);
-
-    return Buffer.concat([
-        length,
-        message
-    ]);
+    return message;
 };
 
-const decodeMessage = (data: Buffer): Message => {
-    const length = data.readUIntBE(0, 4);
-    const cborMessage = data.slice(4, 4 + length);
-
+const decodeMessage = (cborMessage: Buffer): Message => {
     try {
         const message = cbor.decode(cborMessage);
 
         return message;
     } catch (err) {
-        log.warn(`Failed to decode cbor with expected length of ${length} but actual length of ${data.byteLength - 4}`);
+        log.warn('Failed to decode cbor');
 
         throw err;
     }
