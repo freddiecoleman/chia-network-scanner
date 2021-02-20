5ee42e95b876bfd21204c4eb48c933bd339973ac
Freddie Coleman <f.coleman@hotmail.co.uk>
2021-02-20 22:49:42 +0000

    encoder/decoder for handshake message

--- encoder.ts
+++ encoder.ts
@@ -1,8 +1,15 @@
-import cbor from 'cbor';
 import { Peer } from './peer';
 import { log } from './log';
 import { Message } from './MessageChannel';
 
+// There will soon be 3 networks.
+type NetworkId = 'testnet' | 'mainnet';
+
+// For now we will just try to keep up to date with the latest version. Should be easy as we do not implement the entire protocol.
+type ProtocolVersion = '0.0.29';
+
+
+// TODO: needs to be replaced by new non cbor decoder
 const decodePeer = (peer: Buffer): Peer => {
     const hostname = peer.slice(0, 4).toString();
     const port = peer.readUIntBE(4, 2);
@@ -15,29 +22,122 @@ const decodePeer = (peer: Buffer): Peer => {
     });
 };
 
-const encodeMessage = (func: string, data: any): Buffer => {
-    const message = cbor.encode({
-        f: func,
-        d: data
-    }) as Buffer;
-
-    return message;
+const ProtocolMessageTypes = {
+    handshake: 1,
+    handshake_ack: 2,
+    request_peers: 40,
+    respond_peers: 41
 };
 
-const decodeMessage = (cborMessage: Buffer): Message => {
-    try {
-        const message = cbor.decode(cborMessage);
+interface Handshake {
+    network_id: NetworkId;
+    protocol_version: ProtocolVersion;
+    software_version: string;
+    server_port: number;
+    node_type: number;
+}
 
-        return message;
-    } catch (err) {
-        log.warn('Failed to decode cbor');
+// TODO: needs to be replaced by non cbor version
+// Data must be any as we dont know what we are encoding
+const encodeMessage = (messageType: number, data: any): Buffer => {
+    if (messageType === ProtocolMessageTypes.handshake) {
+        const { network_id, protocol_version, software_version, server_port, node_type } = data as Handshake;
 
-        throw err;
+        const serverPortBuffer = Buffer.alloc(2);
+
+        serverPortBuffer.writeUInt16BE(server_port);
+
+        return Buffer.concat([
+            Buffer.from([messageType]),
+            encodeString(network_id),
+            encodeString(protocol_version),
+            encodeString(software_version),
+            serverPortBuffer,
+            Buffer.from([node_type]),
+        ]);
     }
+
+    throw new Error(`Could not encode message of type ${messageType}`);
+};
+
+// TODO: needs to be replaced by non cbor version
+const decodeMessage = (message: Buffer): any => {
+    const messageType = message[0];
+
+    if (messageType === ProtocolMessageTypes.handshake) {
+        let currentPos = 1;
+
+        // Extract network_id (bytes32)
+        const network_id = decodeString(message.slice(currentPos)); // testnet | mainnet
+
+        currentPos += 4 + network_id.length;
+
+        const protocol_version = decodeString(message.slice(currentPos));
+
+        currentPos +=  4 + protocol_version.length;
+
+        const software_version = decodeString(message.slice(currentPos));
+
+        currentPos += 4 + software_version.length;
+
+        const server_port = message.readUInt16BE(currentPos);
+
+        currentPos += 2;
+
+        const node_type = message[currentPos];
+
+        return {
+            network_id,
+            protocol_version,
+            software_version,
+            server_port,
+            node_type
+        };
+    }
+
+    log.warn(`Could not decode message of type ${messageType}`);
+
+    return null;
+};
+
+
+// https://github.com/Chia-Network/chia-blockchain/blob/main/src/util/streamable.py#L206
+const decodeString = (buffer: Buffer): string => {
+    // Extra length of string from first 4 bytes
+    const str_size_bytes = buffer.readUIntBE(0, 4);
+
+    if (str_size_bytes === 0) {
+        throw new Error('EOF while decoding string size');
+    }
+
+    const stringBuffer = buffer.slice(4, 4 + str_size_bytes);
+    const result = stringBuffer.toString('utf-8');
+
+    if (!result) {
+        throw new Error('EOF while decoding string');
+    }
+
+    return result;
+};
+
+// https://github.com/Chia-Network/chia-blockchain/blob/main/src/util/streamable.py#L206
+const encodeString = (str: string): Buffer => {
+    const str_size_bytes = str.length;
+    const strBuffer = Buffer.alloc(4 + str_size_bytes);
+
+    // Encode length of string in first 4 bytes
+    strBuffer.writeIntBE(str_size_bytes, 0, 4);
+
+    // Encode the actual string
+    strBuffer.write(str, 4, 'utf-8');
+
+    return strBuffer;
 };
 
 export {
-    decodePeer,
     encodeMessage,
-    decodeMessage
+    decodeMessage,
+    decodePeer,
+    encodeString,
+    decodeString
 };
--- test/encoder.spec.ts
+++ test/encoder.spec.ts
@@ -1,35 +1,59 @@
-import { decodeMessage, encodeMessage } from '../encoder';
+import { decodeMessage } from '../encoder';
+import { encodeMessage } from '../encoder';
+import { decodeString, encodeString } from '../encoder';
 
 describe('encoder', () => {
+    describe('encodeString', () => {
+        it('encodes a string', () => {
+            const encodedString = encodeString('Chia Network');
+
+            expect(encodedString.toString('hex')).toMatchInlineSnapshot(
+                `"0000000c43686961204e6574776f726b"`
+            );
+        });
+
+        it('decodes a string', () => {
+            const encodedString = encodeString('Chia Network');
+            const decodedString = decodeString(encodedString);
+
+            expect(decodedString).toEqual('Chia Network');
+        });
+    });
+
     describe('encodeMessage', () => {
-        it('encodes a message', () => {
-            const encodedMessage = encodeMessage('give_monies_please', {
-                amount: 1000000,
+        it('encodes handshake message', () => {
+            const encodedHandshake = encodeMessage(1, {
+                network_id: 'testnet',
+                protocol_version: '0.0.29',
+                software_version: '1.0rc2',
+                server_port: 58444,
+                node_type: 1,
             });
 
-            expect(encodedMessage.toString('hex')).toMatchInlineSnapshot(
-                `"00000025a2616672676976655f6d6f6e6965735f706c656173656164a166616d6f756e741a000f4240"`
+            expect(encodedHandshake.toString('hex')).toMatchInlineSnapshot(
+                `"0100000007746573746e657400000006302e302e323900000006312e30726332e44c01"`
             );
         });
     });
 
-    describe('decodeMessage', () => {
-        it('decodes a message', () => {
-            const decodedMessage = decodeMessage(
-                Buffer.from(
-                    '00000025a2616672676976655f6d6f6e6965735f706c656173656164a166616d6f756e741a000f4240',
-                    'hex'
-                )
-            );
+    describe('decodeMessages', () => {
+        it('decodes handshake message', () => {
+            const encodedHandshake = encodeMessage(1, {
+                network_id: 'testnet',
+                protocol_version: '0.0.29',
+                software_version: '1.0rc2',
+                server_port: 58444,
+                node_type: 1,
+            });
+            const decodedHandshake = decodeMessage(encodedHandshake);
 
-            expect(decodedMessage).toMatchInlineSnapshot(`
-                Object {
-                  "d": Object {
-                    "amount": 1000000,
-                  },
-                  "f": "give_monies_please",
-                }
-            `);
+            expect(decodedHandshake).toEqual({
+                network_id: 'testnet',
+                protocol_version: '0.0.29',
+                software_version: '1.0rc2',
+                server_port: 58444,
+                node_type: 1,
+            });
         });
     });
 });
